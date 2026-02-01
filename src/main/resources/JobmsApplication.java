a86aef33d261fcb19295002f17037530dd714337
Kumari Sonika <kumarisonika1929@gmail.com>
2026-02-02 02:36:44 +0530

    eureka integrated

--- src/main/resources/JobmsApplication.java
+++ src/main/resources/JobmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobmsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JobmsApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JobmsApplication.class, args);
+	}
 
 }
